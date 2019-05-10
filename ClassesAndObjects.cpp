0b80ac867185121fbf38ed5aea1d04848a9dadc2
ApekshitPagare <44097207+ApekshitPagare@users.noreply.github.com>
2019-05-10 17:58:11 +0530

    Update ClassesAndObjects.cpp

--- ClassesAndObjects.cpp
+++ ClassesAndObjects.cpp
@@ -4,15 +4,15 @@ using namespace std;
 class MyClass {
     int a;
     public:
-        MyClass() {
+        MyClass() {                                                 /*Default Constructor*/
             a = 0;
             cout<<"in constructor"<<endl;
         }
-        MyClass(int b) {
+        MyClass(int b) {                                            /*Parametrised Constructor*/
             a = b;
             cout<<"in parameterised constructor"<<endl;
         }
-        MyClass(MyClass &obj) {
+        MyClass(MyClass &obj) {                                     /*Copy Constructor,Obj 2 copies value of its 'a' using address of 'a' from obj1*/
             a = obj.a;
             cout<<"in copy constructor"<<endl;
         }
@@ -22,13 +22,13 @@ class MyClass {
         void display() {
             cout<<"a: "<<a<<endl;
         }
-        ~MyClass() {
+        ~MyClass() {                                               /*Destructor*/
             cout<<"in destructor - "<<a<<endl;
         }
 };
 
 int main() {
-
+   `/*MyClass obj=MyClass()*/                                       /*Links to default Constructor*/
     MyClass obj1(10);
     MyClass obj2(obj1);
     MyClass obj3;
